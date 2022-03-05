8a79e71c5d8796705de1f44c6a8b35c63a85c80e
borobor <bornajurisa@gmail.com>
2022-03-05 17:48:59 +0100

    Add user prompt for custom grid dimensions

--- js/etch.js
+++ js/etch.js
@@ -6,21 +6,42 @@ for (i = 1; i <= 256; i++) {
 	container.appendChild(div);
 }
 
-const gridItems = document.querySelectorAll(".grid-item");
-
-gridItems.forEach(item => {
-	item.addEventListener('mouseover', () => {
-		item.classList.add("color");
-	});
-});
+let gridItems = document.querySelectorAll(".grid-item");
+draw();
 
 const resetButton = document.querySelector(".reset");
 resetButton.addEventListener('click', () => {
-	resetGrid();
+	createNewGrid();
 });
+
+
+function createNewGrid() {
+	userPrompt = +prompt("Enter new grid size:", "60 max");
+	if (userPrompt > 60 || isNaN(userPrompt) || userPrompt == 0) return;
+	while (container.firstChild) {
+		container.removeChild(container.lastChild);
+	}
+	for (i = 1; i <= userPrompt**2; i++) {
+		const div = document.createElement('div');
+		div.classList.add("grid-item");
+		container.appendChild(div);
+	}
+	gridItems = document.querySelectorAll(".grid-item");
+	container.style.cssText = `grid-template-columns: repeat(${userPrompt}, auto);`;
+	resetGrid();
+	draw();
+};
 
 function resetGrid() {
 	gridItems.forEach(item => {
 		item.classList.remove("color");
 	})
 };
+
+function draw() {
+gridItems.forEach(item => {
+	item.addEventListener('mouseover', () => {
+		item.classList.add("color");
+	});
+});
+}
