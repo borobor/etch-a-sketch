ee66ce57270d9f32130dea9545b88a3207a431fb
borobor <bornajurisa@gmail.com>
2022-03-05 12:58:43 +0100

    Add mouseover eventListener to divs to color them

--- js/etch.js
+++ js/etch.js
@@ -2,6 +2,11 @@ const container = document.querySelector("#grid-container");
 
 for (i = 1; i <= 256; i++) {
 	const div = document.createElement('div');
+	div.classList.add("grid-item");
 	div.textContent = i;
 	container.appendChild(div);
+
+	div.addEventListener('mouseover', () => {
+		div.classList.add("color");
+	});
 }
